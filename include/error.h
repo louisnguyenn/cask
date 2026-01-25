fa1bf65e16f1cfcec1bc0c06ff8ca498847418c0
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-25 17:00:00 -0500

    feat: define error header with enum definition

---
+++ include/error.h
@@ -0,0 +1,12 @@
+#ifndef ERROR_H
+#define ERROR_H
+typedef enum
+{
+    CASK_OK = 0,    // operation success (0 = success, -1 = error)
+    CASK_ERR_IO,    // file read/write error
+    CASK_ERR_NOT_FOUND, // key not found
+    CASK_ERR_INVALID_FORMAT,    // invalid format (file or record corrupted)
+    CASK_ERR_FULL   // storage is full
+} cask_error_t;
+
+#endif // ERROR_H
--- src/error.c
+++
@@ -1,8 +0,0 @@
-typedef enum
-{
-    CASK_OK = 0,
-    CASK_ERR_IO,
-    CASK_ERR_NOT_FOUND,
-    CASK_ERR_INVALID_FORMAT,
-    CASK_ERR_FULL
-} cask_error_t;
