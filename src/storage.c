3d6d6f55f0dd3c44f17954d0b7130ef62d1b1f3a
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-26 11:28:37 -0500

    feat: successfully initialized storage

--- src/storage.c
+++ src/storage.c
@@ -6,7 +6,7 @@ cask_error_t cask_storage_init(const char *filename, uint32_t max_records)
     cask_header_t header;
     char *key = NULL;
     char *value = NULL;
-    long file_size = 0;
+    unsigned long file_size = 0;
     long pos = 0;
 
     fptr = fopen(filename, "rb"); // open file in read mode (read binary)
@@ -25,8 +25,7 @@ cask_error_t cask_storage_init(const char *filename, uint32_t max_records)
 
         // initalize header
         memcpy(header.magic, "CSK1", sizeof(char) * MAGIC_STRING_SIZE);
-        header.magic[5] = '\0';               // null terminate
-        header.version = CASK_FORMAT_VERSION; // version 1
+        header.version = CASK_FORMAT_VERSION;          // version 1
         header.max_records = max_records;
         header.record_size = sizeof(cask_record_t);
 
@@ -48,27 +47,35 @@ cask_error_t cask_storage_init(const char *filename, uint32_t max_records)
         // get file size
         fseek(fptr, 0, SEEK_END);
         file_size = ftell(fptr);
-        if (file_size < max_records) // check if file is big enough to write a header
+        if (file_size < sizeof(cask_header_t)) // check if file is big enough to write a header
         {
+            // printf('filesize: %llu', file_size);
+            // printf('size of header: %llu', sizeof(cask_header_t));
             return CASK_ERR_INVALID_FORMAT;
         }
 
         fseek(fptr, 0, SEEK_SET);                // seek to the beginning of the file
         fread(&header, sizeof(header), 1, fptr); // read struct
-
         // read magic number identifier
-        if (strcmp(header.magic, "CSK1") != 0)
+        // printf("header magic: %s\n", header.magic);
+        if (memcmp(header.magic, "CSK1", MAGIC_STRING_SIZE) != 0)
         {
+            printf("magic error\n");
             return CASK_ERR_INVALID_FORMAT;
         }
 
         // read version
+        printf("header version: %d\n", header.version);
         if (header.version != CASK_FORMAT_VERSION)
         {
+            printf("header error\n");
             return CASK_ERR_INVALID_FORMAT;
         }
     }
 
     fclose(fptr);
+    free(key);
+    free(value);
+
     return CASK_OK;
 }
