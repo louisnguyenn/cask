01c8c6b646ebc95cae57b1f6a99dee41b15beb52
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-26 10:09:48 -0500

    feat: update header with size definitions and additional functions

--- include/storage.h
+++ include/storage.h
@@ -1,25 +1,33 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+#define MAGIC_STRING_SIZE 5
+#define KEY_SIZE 32
+#define VALUE_SIZE 128
+#define CASK_FORMAT_VERSION 1   // define version
+
 #include <stdint.h>
 #include "error.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * define storage file layout 
  */
-struct cask_header_t {
-    char magic_number[4];
+typedef struct {
+    char magic[MAGIC_STRING_SIZE] = "CSK1";
     uint32_t version;
     uint32_t max_records;
     uint32_t record_size;
-};
+}  cask_header_t;
 
 /**
  * each record contains the following
  */
-struct cask_record_t {
-    char key[32];
-    char value[128];
+typedef struct {
+    char key[KEY_SIZE];
+    char value[VALUE_SIZE];
     bool flag;
-};
+} cask_record_t;
 
 // function prototypes
 
@@ -27,3 +35,10 @@ struct cask_record_t {
  * initialize storage function
  */
 cask_error_t cask_storage_init(const char *filename, uint32_t max_records);
+
+/**
+ * HELPER FUNCTION: returns position/offset of a record
+ */
+long cask_record_offset(uint32_t index);
+
+#endif // STORAGE_H
