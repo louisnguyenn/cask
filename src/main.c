1b25fc8d484dc0148dc3f60f8f660f55ce9fb2ba
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-26 18:01:30 -0500

    fix: change filename to correct directory

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main()
     cask_error_t err;
     uint32_t max_records = 100;
 
-    err = cask_storage_init("data/store.bin", max_records); // initialize storage
+    err = cask_storage_init("../data/store.bin", max_records); // initialize storage
 
     if (err != CASK_OK)
     {
