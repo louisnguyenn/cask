6a4dff74181bf0470d031961fc0de18836de335f
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-30 18:14:01 -0500

    docs: describe record struct variables

--- include/storage.h
+++ include/storage.h
@@ -3,7 +3,7 @@
 #define MAGIC_STRING_SIZE 4
 #define KEY_SIZE 32
 #define VALUE_SIZE 128
-#define CASK_FORMAT_VERSION 1   // define version
+#define CASK_FORMAT_VERSION 1 // define version
 
 #include <stdint.h>
 #include "error.h"
@@ -12,22 +12,24 @@
 #include <stdlib.h>
 
 /**
- * define storage file layout 
+ * define storage file layout
  */
-typedef struct {
-    char magic[MAGIC_STRING_SIZE];  // file signature (magic number) makes sure it reads the correct file as a database
-    uint32_t version;               // file format version (future-proofing, when changing the record structure)
-    uint32_t max_records;           // max number of records the file supports
-    uint32_t record_size;           // size (in bytes) of one cask_record_t
-}  cask_header_t;
+typedef struct
+{
+    char magic[MAGIC_STRING_SIZE]; // file signature (magic number) makes sure it reads the correct file as a database
+    uint32_t version;              // file format version (future-proofing, when changing the record structure)
+    uint32_t max_records;          // max number of records the file supports
+    uint32_t record_size;          // size (in bytes) of one cask_record_t
+} cask_header_t;
 
 /**
  * each record contains the following
  */
-typedef struct {
-    uint32_t in_use;
-    char key[KEY_SIZE];
-    char value[VALUE_SIZE];
+typedef struct
+{
+    uint32_t in_use;        // flag for empty slot to place a new record
+    char key[KEY_SIZE];     // record key ('user1')
+    char value[VALUE_SIZE]; // record value (name, username, email, etc.)
 } cask_record_t;
 
 // function prototypes
