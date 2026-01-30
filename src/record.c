fef716b0755e942b35f297b16b1a9afd1b638170
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-30 18:11:31 -0500

    feat: begin implementation of creating a record with opening the file and reading the header

--- src/record.c
+++ src/record.c
@@ -2,7 +2,22 @@
 
 cask_error_t cask_record_put(const char *key, const char *value)
 {
+    FILE *fptr;
+    cask_record_t record;
+    cask_header_t header;
+    uint32_t curr_record = 0;
 
+    fptr = fopen("../data/store.bin", "rb+");   // open in write binary mode
+
+    fread(&header, sizeof(cask_header_t), 1, fptr); // read the header
+
+    // read the record
+    for (int i = 0; i < header.max_records; i += 1)
+    {
+        fread(&record, )
+    }
+
+    return CASK_OK;
 }
 
 cask_error_t cask_record_get(const char *key, char *out_value)
