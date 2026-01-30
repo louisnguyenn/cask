7916c89c729215b75fa467378c1d613d400891a3
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-30 18:10:42 -0500

    feat: create skeleton of all CRUD functions and offset calculation

--- src/record.c
+++ src/record.c
@@ -2,5 +2,25 @@
 
 cask_error_t cask_record_put(const char *key, const char *value)
 {
-    
+
+}
+
+cask_error_t cask_record_get(const char *key, char *out_value)
+{
+
+}
+
+cask_error_t cask_record_delete(const char *key)
+{
+
+}
+
+cask_error_t cask_storage_close()
+{
+
+}
+
+long cask_record_offset(uint32_t index)
+{
+
 }
