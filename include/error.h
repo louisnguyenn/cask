9bee870e8ac3d6e9d5ee15f35a41655ddc90d4fa
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-31 15:06:20 -0500

    feat: add new error in enum for user validation

--- include/error.h
+++ include/error.h
@@ -7,13 +7,14 @@
 #include <stdbool.h>
 typedef enum
 {
-    CASK_OK = 0,             // operation success (0 = success, -1 = error)
-    CASK_ERR_IO,             // file read/write error
-    CASK_ERR_NOT_FOUND,      // key not found
-    CASK_ERR_INVALID_FORMAT, // invalid format (file or record corrupted)
-    CASK_ERR_FULL,           // storage is full
-    CASK_ERR_KEY_TOO_LARGE,  // record key is too large
-    CASK_ERR_VALUE_TOO_LARGE // record value is too large
+    CASK_OK = 0,              // operation success (0 = success, -1 = error)
+    CASK_ERR_IO,              // file read/write error
+    CASK_ERR_NOT_FOUND,       // key not found
+    CASK_ERR_INVALID_FORMAT,  // invalid format (file or record corrupted)
+    CASK_ERR_FULL,            // storage is full
+    CASK_ERR_KEY_TOO_LARGE,   // record key is too large
+    CASK_ERR_VALUE_TOO_LARGE, // record value is too large
+    CASK_ERR_INVALID_INPUT    // invalid input from user
 } cask_error_t;
 
 const char *cask_strerror(cask_error_t err);
