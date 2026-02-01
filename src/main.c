fb9af99f866cea5991aa492c328c5bf11e72e0a6
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-31 20:32:27 -0500

    feat: create database initialization validation

--- src/main.c
+++ src/main.c
@@ -4,12 +4,7 @@
 
 /**
  * TODO:
- * 1. create a "isdigit" function to check if the user input is an integer
- * if not then return -1 if not successful, then check if the return type is equal to CASK_OK
- * if it return 0, then it is validated because CASK_OK is equal to 0
- * if not then inform the user of the error
- * 
- * 2. create a new error message in the error and c file to inform the user to initialize the database first before choosing the other options
+ * 1. create a new error message in the error and c file to inform the user to initialize the database first before choosing the other options
  */
 int main()
 {
@@ -31,8 +26,8 @@ int main()
         printf("Enter your choice: ");
 
         scanf("%d", &input);
-        err = isdigit(input);
-        if (!err) // validate input
+        err = validate_input(input);
+        if (err != CASK_OK) // validate input
         {
             printf("Error: %s", cask_strerror(err));
         }
@@ -43,21 +38,23 @@ int main()
             printf("Enter the number of max records: ");
             scanf("%d", &max_records);
 
-            err = isdigit(max_records);
-            if (!err)
+            err = validate_input(max_records);
+            if (err != CASK_OK)
             {
                 printf("Error: %s\n", cask_strerror(err));
             }
 
             err = cask_storage_init("../data/store.bin", max_records); // initialize storage
+
             if (err != CASK_OK)
             {
                 printf("Initialization failed: %s\n", cask_strerror(err));
-                return -1;
             }
-
-            printf("Storage initialized successfully\n");
-            init_storage_flag = 1;
+            else
+            {
+                printf("Storage initialized successfully\n");
+                init_storage_flag = 1;
+            }
             break;
         case 2:
             if (init_storage_flag != 1)
