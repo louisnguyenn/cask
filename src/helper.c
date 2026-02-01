af01c320dd876e6ee3e5ffbf535846c7889595a0
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-31 20:12:25 -0500

    chore: create helper file with helper functions

---
+++ src/helper.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+
+/**
+ * validate user input
+ */
+int validate_input(int input)
+{
+
+}
+
+/**
+ * calculate record offsets
+ */
+long cask_record_offset(uint32_t index, uint32_t header_size, uint32_t record_size)
+{
+    long offset = header_size + (index * record_size);
+
+    return offset;
+}
