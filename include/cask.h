9b2579ea662f62135851ccd6f280f4700d2b48eb
Louis Nguyen <louis.nguyen550@gmail.com>
2026-01-31 20:06:48 -0500

    refactor: encapsulate the api and engine header files

---
+++ include/cask.h
@@ -0,0 +1,37 @@
+#ifndef CASK_H
+#define CASK_H
+
+#include <stdint.h>
+#include "error.h"
+
+/**
+ * initialize storage function
+ * parameters: filename/name of the database, max number of records
+ */
+cask_error_t cask_storage_init(const char *filename, uint32_t max_records);
+
+/**
+ * create record function
+ * parameters: record key, values to put in the record
+ */
+cask_error_t cask_record_put(const char *key, const char *value);
+
+/**
+ * get a record function
+ * parameters: record key, values of the record
+ */
+cask_error_t cask_record_get(const char *key, char *out_value);
+
+/**
+ * delete a record function
+ * parameters: record key
+ */
+cask_error_t cask_record_delete(const char *key);
+
+/**
+ * clean/close the storage function
+ * parameters: none
+ */
+cask_error_t cask_storage_close();
+
+#endif  // CASK_H
