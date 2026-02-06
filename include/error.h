a1baeb3e4450e7f583b3e63c3d4b6220b4069def
Louis Nguyen <louis.nguyen550@gmail.com>
2026-02-06 18:36:11 -0500

    feat: add record in use enum

--- include/error.h
+++ include/error.h
@@ -6,16 +6,17 @@
 #include <stdio.h>
 #include <string.h>
 typedef enum {
-    CASK_OK = 0,              // operation success (0 = success, -1 = error)
-    CASK_ERR_IO,              // file read/write error
-    CASK_ERR_NOT_FOUND,       // key not found
-    CASK_ERR_INVALID_FORMAT,  // invalid format (file or record corrupted)
-    CASK_ERR_FULL,            // storage is full
-    CASK_ERR_KEY_TOO_LARGE,   // record key is too large
-    CASK_ERR_VALUE_TOO_LARGE, // record value is too large
-    CASK_ERR_INVALID_INPUT,   // invalid input from user
-    CASK_ERR_INIT_STORAGE,    // database is not yet initialized
-    CASK_ERR_RECORD_NOT_FOUND // record not found
+    CASK_OK = 0,               // operation success (0 = success, -1 = error)
+    CASK_ERR_IO,               // file read/write error
+    CASK_ERR_NOT_FOUND,        // key not found
+    CASK_ERR_INVALID_FORMAT,   // invalid format (file or record corrupted)
+    CASK_ERR_FULL,             // storage is full
+    CASK_ERR_KEY_TOO_LARGE,    // record key is too large
+    CASK_ERR_VALUE_TOO_LARGE,  // record value is too large
+    CASK_ERR_INVALID_INPUT,    // invalid input from user
+    CASK_ERR_INIT_STORAGE,     // database is not yet initialized
+    CASK_ERR_RECORD_NOT_FOUND, // record not found
+    CASK_ERR_RECORD_IN_USE     // record in use
 } cask_error_t;
 
 const char* cask_strerror(cask_error_t err);
