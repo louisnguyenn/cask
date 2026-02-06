cfc0855c72709f2682745d2bb610772fb966fbef
Louis Nguyen <louis.nguyen550@gmail.com>
2026-02-06 18:38:29 -0500

    refactor: update naming convention for empty record enum

--- include/error.h
+++ include/error.h
@@ -16,7 +16,7 @@ typedef enum {
     CASK_ERR_INVALID_INPUT,    // invalid input from user
     CASK_ERR_INIT_STORAGE,     // database is not yet initialized
     CASK_ERR_RECORD_NOT_FOUND, // record not found
-    CASK_ERR_RECORD_IN_USE     // record in use
+    CASK_ERR_EMPTY_RECORD      // record in use
 } cask_error_t;
 
 const char* cask_strerror(cask_error_t err);
